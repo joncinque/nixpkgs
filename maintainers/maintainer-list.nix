2d2c861cb881ae61913ae6ad7f2e772e26ca1c68
Patrick Reich <patrick@managarmr.de>
2025-12-16 15:13:55 +0100

    maintainers: add Managarmrr

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -16339,6 +16339,13 @@
     githubId = 93316844;
     name = "Manaiki Laut";
   };
+  Managarmrr = {
+    email = "patrick@managarmr.de";
+    github = "Managarmrr";
+    githubId = 63654312;
+    name = "Patrick Reich";
+    keys = [ { fingerprint = "CE12 ED73 C6D5 DE05 1651  6DCC 3977 61D5 D102 CB9D"; } ];
+  };
   mandos = {
     email = "marek.maksimczyk@mandos.net.pl";
     github = "mandos";
