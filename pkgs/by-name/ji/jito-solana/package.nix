e5756ef55ee5b7d598bfd0807afc833e352645c1
Jon C <me@jonc.dev>
2026-03-31 18:25:56 +0200

    jito: Bump to v4.0.0-beta.4

--- pkgs/by-name/ji/jito-solana/package.nix
+++ pkgs/by-name/ji/jito-solana/package.nix
@@ -81,13 +81,13 @@ stdenv.mkDerivation (
   in
   {
     pname = "jito-solana";
-    version = "4.0.0-beta.2";
+    version = "4.0.0-beta.4";
 
     src = fetchFromGitHub {
       owner = "jito-foundation";
       repo = "jito-solana";
       rev = "v${finalAttrs.version}-jito";
-      hash = "sha256-HpAVZ63mGEt++xGEiV4Hnj5vhLj4XLSv31pLAMUR5dA=";
+      hash = "sha256-5vWJSpKv+pSK4NlGmMEsQCFkEoGDVc2qa/q4iQmVdrI=";
       fetchSubmodules = true;
     };
 
