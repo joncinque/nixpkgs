e0a3fc98bbca03d45fe4e3e544a824dd28b28aea
Jon C <me@jonc.dev>
2026-07-22 07:56:07 -0400

    Update hash

--- pkgs/by-name/ji/jito-solana/package.nix
+++ pkgs/by-name/ji/jito-solana/package.nix
@@ -15,7 +15,7 @@ solana-agave.overrideAttrs (
       rev = "v${finalAttrs.version}-jito.1";
       fetchSubmodules = true;
 
-      hash = "sha256-0gkI4DGMjIAmhTNchqfeL6M+7bbxpGgLkZhYbfW6N6o=";
+      hash = "sha256-KmWPjN+awp9ny8+2iriZL/ylNM2YcEJju4ai0WSswXY=";
     };
 
     passthru = lib.attrsets.recursiveUpdate oldAttrs.passthru { solana.jitoSupport = true; };
