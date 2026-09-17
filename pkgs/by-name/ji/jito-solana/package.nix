16dabeccba12954451edf49a773d29662c9e48e3
Jon C <me@jonc.dev>
2026-09-17 18:58:57 +0200

    Bump to v4.3.0-rc.1

--- pkgs/by-name/ji/jito-solana/package.nix
+++ pkgs/by-name/ji/jito-solana/package.nix
@@ -7,7 +7,7 @@
 solana-agave.overrideAttrs (
   finalAttrs: oldAttrs: {
     pname = "jito-solana";
-    version = "4.3.0-rc.0";
+    version = "4.3.0-rc.1";
 
     src = fetchFromGitHub {
       owner = "jito-foundation";
@@ -15,7 +15,7 @@ solana-agave.overrideAttrs (
       rev = "v${finalAttrs.version}-jito";
       fetchSubmodules = true;
 
-      hash = "sha256-tR1erIrR0CsG4NOCfGyoYSSjp6Sxdip66YksPAG8WEY=";
+      hash = "sha256-otelwPHWw0or13vTstM8QSchUQQ2sws7pzpJBMv9Ing=";
     };
 
     passthru = lib.attrsets.recursiveUpdate oldAttrs.passthru { solana.jitoSupport = true; };
